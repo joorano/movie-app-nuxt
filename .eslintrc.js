21a48aee8b5420ae40ca91a05bf1d67ceb82d48f
joorano <jooryeonh@gmail.com>
2021-08-15 15:14:56 +0900

    Create project

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+// module.exports = {
+//   root: true,
+//   env: {
+//     browser: true,
+//     node: true
+//   },
+//   parserOptions: {
+//     parser: '@babel/eslint-parser',
+//     requireConfigFile: false
+//   },
+//   extends: [
+//     '@nuxtjs',
+//     'plugin:nuxt/recommended'
+//   ],
+//   plugins: [
+//   ],
+//   // add your custom rules here
+//   rules: {}
+// }
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
+  },
+  extends: [
+    'plugin:vue/strongly-recommended',
+    'eslint:recommended'
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {
+    'vue/html-closing-bracket-newline': ['error', {
+      singleline: 'never',
+      multiline: 'never'
+    }],
+    'vue/html-self-closing': ['error', {
+      html: {
+        void: 'always',
+        normal: 'never',
+        component: 'always'
+      },
+      svg: 'always',
+      math: 'always'
+    }]
+  }
+}
